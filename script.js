77cfcb1ec618a34bcb5519bab0c2501fa4d91391
Eyobgk <168523335+Eyobgk@users.noreply.github.com>
2024-10-18 18:38:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.transparent-button').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav_links');
+    navLinks.classList.toggle('show'); // Toggle the class to show/hide menu
+});
